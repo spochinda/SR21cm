fix sampler, change normalisation, update training loop

models/loss.py
@@ -8,37 +8,20 @@ class VPLoss:
 
     def __call__(self, net, images, conditionals, labels, augment_pipe=None):
         #with torch.autograd.profiler.record_function("## data prep in loss ##"):
-        rnd_uniform = torch.rand([images.shape[0], 1, 1, 1, 1], device=images.device)
-        t = 1 + rnd_uniform * (self.epsilon_t - 1)
-
-        if net.noise_schedule_opt["schedule_type"]=="EDM": 
-            y, augment_labels = augment_pipe(images) if augment_pipe is not None else (images, None)
-            sigma = self.sigma(t) #These sigmas range from epsilon_t to 152. Doesn't seem right.
-        elif net.noise_schedule_opt["schedule_type"]=="VPSDE":
-            #y, augment_labels = augment_pipe(images) if augment_pipe is not None else (images, None)
-            y, sigma = net.SDE.marginal_prob(x=images, t=t)
-            #print(f"std {sigma.flatten()}", flush=True)
+        #rnd_uniform = torch.rand([images.shape[0], 1, 1, 1, 1], device=images.device)
+        #t = 1 + rnd_uniform * (self.epsilon_t - 1)
+        t = torch.rand(size=(images.shape[0],1,1,1,1), device=images.device) * (1. - self.epsilon_t) + self.epsilon_t
+        z = torch.randn_like(images)
+        mean, std = net.SDE.marginal_prob(x=images, t=t)
+        perturbed_data = mean + std * z
+        x = torch.cat([perturbed_data, *conditionals], dim = 1)
+        score = net.model(x=x, noise_labels=t.flatten(), class_labels=labels, augment_labels=None) # 999 from wrapper get_score_fn
+        score = -score / std #from wrapper get_score_fn
         
-        #with torch.no_grad(): #only track grad for things that go into the model
-        n = torch.randn_like(y) * sigma
-        y = y + n 
-
-        #with torch.autograd.profiler.record_function("## forward ##"):
-        if net.noise_schedule_opt["schedule_type"]=="EDM":
-            weight = 1 / sigma ** 2
-            D_yn = net.vprecond_forward(x=y, conditionals=conditionals, sigma=sigma, class_labels=labels) #vprecond
-            loss = weight * ((D_yn - y) ** 2)
-        elif net.noise_schedule_opt["schedule_type"]=="VPSDE":
-            #print(f"VP loss \nt: {t.flatten()}\nstd: {sigma.flatten()} \ndevice={y.device}", flush=True)
-            x = torch.cat([y, *conditionals], dim = 1)
-            score = net.model(x=x, noise_labels=t.flatten(), class_labels=labels, augment_labels=None)
-            #loss = torch.square(score  * sigma + n)
-            loss = torch.square(sigma * (score + (y - images) / sigma**2) )
-        else:
-            raise NotImplementedError
+        loss = torch.square(score * std + z)
+        #loss = torch.square(std * (score + (perturbed_data - images) / std**2) )
         
         loss = torch.mean(loss)
-
         return loss
     
 

models/samplers.py
@@ -37,7 +37,7 @@ class Sampler():
         time_steps = torch.linspace(1., eps, num_steps, device=x_lr.device)
         step_size = time_steps[0] - time_steps[1]
 
-        x = noise * std
+        x = noise #* std
         x_sequence = [x]
         
         
@@ -49,26 +49,24 @@ class Sampler():
 
             batch_time_step = torch.tensor(b*[time_step], device=x_lr.device).view(b,*[1]*len(d)) #torch.ones(batch_size) * time_step
             #f, g = netG.SDE.sde(x=x,t=batch_time_step) #drift=f diffusion=g
+            
+            #mean, noise_labels = netG.SDE.marginal_prob(x=x, t=batch_time_step)
+            #noise_labels = batch_time_step.flatten() #noise_labels.flatten()
+            X = torch.cat([x, *conditionals, x_lr], dim = 1)
 
-            if False:
-                score = netG.model(x=x, time=batch_time_step, x_lr=x_lr, conditionals=conditionals)
-            else:
-                
-                #mean, noise_labels = netG.SDE.marginal_prob(x=x, t=batch_time_step)
-                #noise_labels = batch_time_step.flatten() #noise_labels.flatten()
-                X = torch.cat([x, *conditionals, x_lr], dim = 1)
-
-                try:
-                    score = netG.model(x=X, noise_labels=batch_time_step.flatten(), class_labels=class_labels, augment_labels=None)
-                except Exception as e:
-                    print(e)
-                    score = netG.model(x=x, time=batch_time_step, x_lr=x_lr, conditionals=conditionals) #old UNet model (maybe time is sigma from marginalprob?)
-                    
-                
-                f, g = netG.SDE.rsde(x=x, t=batch_time_step, score=score)
+            try:
+                score = netG.model(x=X, noise_labels=batch_time_step.flatten(), class_labels=class_labels, augment_labels=None) #999 from wrapper get_score_fn
+                std = netG.SDE.marginal_prob(x=torch.zeros_like(x), t=batch_time_step)[1] #from wrapper get_score_fn
+                score = -score / std #from wrapper get_score_fn
+            except Exception as e:
+                print(e)
+                score = netG.model(x=x, time=batch_time_step, x_lr=x_lr, conditionals=conditionals) #old UNet model (maybe time is sigma from marginalprob?)    
+            
+            
+            f, g = netG.SDE.rsde(x=x, t=batch_time_step, score=score, probability_flow=False)
             
             #x_mean = x - (f - g**2 * score) * step_size #double check signs
-            x_mean = x - f * step_size #- g**2 * score) * step_size #double check signs
+            x_mean = x - f * step_size #double check signs
 
             #x = x_mean + g * torch.sqrt(step_size) * torch.randn_like(x) #double check signs
             x = x_mean + g * torch.sqrt(step_size) * torch.randn_like(x) #double check signs
@@ -109,17 +107,16 @@ class Sampler():
         """
         assert netG.noise_schedule_opt["schedule_type"] == "VPSDE", "Sampler only supports VPSDE schedule type."
         netG.model.eval()
-        x_lr = torch.nn.Upsample(scale_factor=4, mode='trilinear')(x_lr)
+        #x_lr = torch.nn.Upsample(scale_factor=4, mode='trilinear')(x_lr)
 
         b,(*d)  = x_lr.shape #select the last conditional to get the shape (order is delta,vbv)
         batch_time_step = torch.ones(size=(b,*[1]*len(d)),dtype=torch.float32, device=x_lr.device)
         #torch.tensor(b*[1.], device=conditionals[-1].device).view(b,*[1]*len(d))
 
         noise = torch.randn(b, *d, device=conditionals[-1].device)
-        mean, std = netG.SDE.marginal_prob(noise, batch_time_step)
-
+        #mean, std = netG.SDE.marginal_prob(noise, batch_time_step)
         # Create the latent code
-        init_x = noise * std
+        init_x = noise # * std # no std because VPSDE std at t=1 is 1
         shape = init_x.shape
         
         def ode_func(batch_time_step, x):        
@@ -128,23 +125,24 @@ class Sampler():
             
             batch_time_step = torch.ones(size=(b,*[1]*len(d)),dtype=torch.float32, device=x_lr.device) * batch_time_step
 
-            if False:
-                score = netG.model(x=x, time=batch_time_step, x_lr=x_lr, conditionals=conditionals)
-            else:
-                #mean, noise_labels = netG.SDE.marginal_prob(x=x, t=batch_time_step)
-                x, noise_labels = netG.SDE.marginal_prob(x=x, t=batch_time_step)
-                noise_labels = batch_time_step.flatten() #noise_labels.flatten()
-                X = torch.cat([x, *conditionals, x_lr], dim = 1)
-                try:
-                    score = netG.model(x=X, noise_labels=noise_labels, class_labels=class_labels, augment_labels=None)
-                except:
-                    score = netG.model(x=x, time=batch_time_step, x_lr=x_lr, conditionals=conditionals) #old UNet model (maybe time is sigma from marginalprob?)
-
-            drift, g = netG.SDE.sde(x=x,t=batch_time_step)
-            g = g.cpu().numpy()
-            drift = drift.cpu().numpy()
-            score = score.cpu().numpy()
-            return  (drift - 0.5 * (g**2) * score).reshape((-1,)).astype(np.float32) #score
+            #mean, noise_labels = netG.SDE.marginal_prob(x=x, t=batch_time_step)
+            #x, noise_labels = netG.SDE.marginal_prob(x=x, t=batch_time_step) ########???
+            noise_labels = batch_time_step.flatten() #noise_labels.flatten()
+            X = torch.cat([x, *conditionals, x_lr], dim = 1)
+            
+            try:
+                score = netG.model(x=X, noise_labels=noise_labels, class_labels=class_labels, augment_labels=None)
+            except:
+                score = netG.model(x=x, time=batch_time_step, x_lr=x_lr, conditionals=conditionals) #old UNet model (maybe time is sigma from marginalprob?)
+            std = netG.SDE.marginal_prob(torch.zeros_like(x), noise_labels)[1] #from wrapper get_score_fn
+            score = -score / std #from wrapper get_score_fn
+            
+            drift = netG.SDE.rsde(x=x, t=batch_time_step, score=score, probability_flow=True)[0]
+            #drift, g = netG.SDE.sde(x=x,t=batch_time_step)
+            #g = g.cpu().numpy()
+            drift = drift.detach().cpu().numpy()
+            #score = score.cpu().numpy()
+            return  drift.reshape((-1,)).astype(np.float32) #(drift - 0.5 * (g**2) * score).reshape((-1,)).astype(np.float32) #score
         
         # Run the black-box ODE solver.
         res = integrate.solve_ivp(fun=ode_func, t_span=(1., eps), y0=init_x.reshape(-1).cpu().numpy(), rtol=rtol, atol=atol, method='RK45')  

models/sde_lib.py
@@ -34,11 +34,13 @@ class VPSDE():
     diffusion = torch.sqrt(beta_t)
     return drift, diffusion
   
-  def rsde(self, x, t, score):
+  def rsde(self, x, t, score, probability_flow=False):
     """Create the drift and diffusion functions for the reverse SDE/ODE."""
     drift, diffusion = self.sde(x, t)
     #score = self.score_fn(t, model_output)
-    drift = drift - diffusion ** 2 * score
+    drift = drift - diffusion ** 2 * score * (0.5 if probability_flow else 1.)
+    # Set the diffusion function to zero for ODEs.
+    diffusion = torch.zeros_like(diffusion) if probability_flow else diffusion
     return drift, diffusion
   
   def marginal_prob(self, x, t):

models/train.py
@@ -109,8 +109,12 @@ def train_step(netG, epoch, train_dataloader, volume_reduction = False, split_ba
     for i,(T21, delta, vbv, T21_lr, labels) in enumerate(train_dataloader):
 
         if volume_reduction:
-            cut_factor = torch.randint(low=1, high=3, size=(1,), device=device)#.item()
-            
+            if netG.network_opt["label_dim"] > 0:
+                cut_factor = torch.randint(low=1, high=3, size=(1,), device=device)#.item()
+            else:
+                cut_factor = torch.randint(low=2, high=3, size=(1,), device=device)#always cut 2
+                #cut_factor = torch.randint(low=1, high=2, size=(1,), device=device)#always cut 1
+
             T21 = get_subcubes(cubes=T21, cut_factor=cut_factor)
             delta = get_subcubes(cubes=delta, cut_factor=cut_factor)
             vbv = get_subcubes(cubes=vbv, cut_factor=cut_factor)
@@ -127,25 +131,29 @@ def train_step(netG, epoch, train_dataloader, volume_reduction = False, split_ba
         else:
             labels = None
         
-        with torch.no_grad():
-            T21_lr_min = torch.amin(T21_lr, dim=(1,2,3,4), keepdim=True)
-            T21_lr_max = torch.amax(T21_lr, dim=(1,2,3,4), keepdim=True)
-            T21_lr = torch.nn.Upsample(scale_factor=4, mode='trilinear')(T21_lr)
+        #with torch.no_grad():
+        #T21_lr_min = torch.amin(T21_lr, dim=(1,2,3,4), keepdim=True)
+        #T21_lr_max = torch.amax(T21_lr, dim=(1,2,3,4), keepdim=True)
+        T21_lr_mean = torch.mean(T21_lr, dim=(1,2,3,4), keepdim=True)
+        T21_lr_std = torch.std(T21_lr, dim=(1,2,3,4), keepdim=True)
+        T21_lr = torch.nn.Upsample(scale_factor=4, mode='trilinear')(T21_lr)
         
-        T21_lr, T21_lr_extrema = normalize(T21_lr)
-        T21, T21_extrema = normalize(T21, x_min=T21_lr_min, x_max=T21_lr_max)
-        delta, delta_extrema = normalize(delta)
-        vbv, vbv_extrema = normalize(vbv)
+        T21_lr, T21_lr_stats = normalize(T21_lr, mode="standard")
+        T21, T21_stats = normalize(T21, mode="standard", x_mean=T21_lr_mean, x_std=T21_lr_std)
+        delta, delta_extrema = normalize(delta, mode="standard")
+        vbv, vbv_extrema = normalize(vbv, mode="standard")
         T21, delta, vbv , T21_lr = augment_dataset(T21, delta, vbv, T21_lr, n=1) #support device
-
         if split_batch: #split subcube minibatch into smaller mini-batches for memory
             sub_data = torch.utils.data.TensorDataset(T21, delta, vbv, T21_lr)
-            sub_dataloader = torch.utils.data.DataLoader(sub_data, batch_size=(2**(cut_factor.item()-1))**3, shuffle=False, sampler = None) 
-            
+            sub_dataloader = torch.utils.data.DataLoader(sub_data, batch_size=4, shuffle=False, sampler = None) # (2**(cut_factor.item()-1))**3 // 2 #4
+            if False:#str(device)=="cuda:0":
+                print("Splitting batch...", flush=True)            
             for j,(T21, delta, vbv, T21_lr) in enumerate(sub_dataloader):
+
                 #if str(device)=="cuda:0":
                 #    print(f"Shape of T21: {T21.shape}", flush=True)
-
+                
+                netG.optG.zero_grad()
                 loss = netG.loss_fn(net=netG, images=T21, conditionals=[delta, vbv, T21_lr],
                                     labels=labels, augment_pipe=None,
                                     )
@@ -154,10 +162,11 @@ def train_step(netG, epoch, train_dataloader, volume_reduction = False, split_ba
                 loss.backward()
                 #with torch.autograd.profiler.record_function("## optimizer ##") if False else contextlib.nullcontext():            
                 torch.nn.utils.clip_grad_norm_(netG.model.parameters(), 1.0)
-                netG.optG.zero_grad()
                 netG.optG.step()        
-                #with torch.autograd.profiler.record_function("## ema ##") if False else contextlib.nullcontext():                
                 netG.ema.update() #Update netG.model with exponential moving average
+                
+                #with torch.autograd.profiler.record_function("## ema ##") if False else contextlib.nullcontext():                
+                
         else:
             loss = netG.loss_fn(net=netG, images=T21, conditionals=[delta, vbv, T21_lr],
                                     labels=labels, augment_pipe=None,
@@ -176,6 +185,8 @@ def train_step(netG, epoch, train_dataloader, volume_reduction = False, split_ba
         if (str(device)=="cuda:0") or (str(device)=="cpu"):
             if False: #i%(len(train_data)//16) == 0:
                 print(f"Batch {i} of {len(train_data)} batches")
+
+        break #only do one box for now
     
     if multi_gpu:
         torch.distributed.all_reduce(tensor=avg_loss, op=torch.distributed.ReduceOp.SUM) #total loss=sum(average total batch loss per gpu)
@@ -301,7 +312,8 @@ def validation_step_v2(netG, validation_dataloader, split_batch = True, device="
     for i,(T21, delta, vbv, T21_lr, labels) in tqdm(enumerate(validation_dataloader), desc='validation loop', total=len(validation_dataloader), disable=False if str(device)=="cuda:0" else True):
         # Rest of the code
         # alternating cut_factor 1 and 2:
-        cut_factor = torch.tensor([i%2 + 1], device=device) #randomness comes from validation_dataloader shuffle so it is not the same cubes cut each time
+        #cut_factor = torch.tensor([i%2 + 1], device=device) #randomness comes from validation_dataloader shuffle so it is not the same cubes cut each time
+        cut_factor = torch.tensor([1], device=device) #randomness comes from validation_dataloader shuffle so it is not the same cubes cut each time
         
         T21 = get_subcubes(cubes=T21, cut_factor=cut_factor)
         delta = get_subcubes(cubes=delta, cut_factor=cut_factor)
@@ -317,14 +329,16 @@ def validation_step_v2(netG, validation_dataloader, split_batch = True, device="
         else:
             labels = None
         
-        T21_lr_min = torch.amin(T21_lr, dim=(1,2,3,4), keepdim=True)
-        T21_lr_max = torch.amax(T21_lr, dim=(1,2,3,4), keepdim=True)
+        #T21_lr_min = torch.amin(T21_lr, dim=(1,2,3,4), keepdim=True)
+        #T21_lr_max = torch.amax(T21_lr, dim=(1,2,3,4), keepdim=True)
+        T21_lr_mean = torch.mean(T21_lr, dim=(1,2,3,4), keepdim=True)
+        T21_lr_std = torch.std(T21_lr, dim=(1,2,3,4), keepdim=True)
         T21_lr = torch.nn.Upsample(scale_factor=4, mode='trilinear')(T21_lr)
         
-        T21_lr, T21_lr_extrema = normalize(T21_lr)
-        T21, T21_extrema = normalize(T21, x_min=T21_lr_min, x_max=T21_lr_max)
-        delta, delta_extrema = normalize(delta)
-        vbv, vbv_extrema = normalize(vbv)
+        T21_lr, T21_lr_extrema = normalize(T21_lr, mode="standard")
+        T21, T21_extrema = normalize(T21, mode="standard", x_mean=T21_lr_mean, x_std=T21_lr_std)
+        delta, delta_extrema = normalize(delta, mode="standard")
+        vbv, vbv_extrema = normalize(vbv, mode="standard")
         T21, delta, vbv , T21_lr = augment_dataset(T21, delta, vbv, T21_lr, n=1) #support device
     
 
@@ -339,8 +353,8 @@ def validation_step_v2(netG, validation_dataloader, split_batch = True, device="
                     MSE_i = MSE_j
                 else:
                     MSE_i = torch.cat([MSE_i, MSE_j], dim=0)
-                if i == 1:
-                    break
+                if i == 0:
+                    break #only do one subbatch for now
         
         else:
             T21_pred = netG.sample.Euler_Maruyama_sampler(netG=netG, x_lr=T21_lr, conditionals=[delta, vbv], class_labels=labels, num_steps=100, eps=1e-3, clip_denoised=False, verbose=False)
@@ -350,6 +364,8 @@ def validation_step_v2(netG, validation_dataloader, split_batch = True, device="
             MSE = MSE_i
         else:
             MSE = torch.cat([MSE, MSE_i], dim=0)
+
+        break #only do one box for now
     
     if multi_gpu:
         MSE_tensor_list = [torch.zeros_like(MSE) for _ in range(torch.distributed.get_world_size())]
@@ -388,8 +404,8 @@ def main(rank, world_size=0, total_epochs = 1, batch_size = 4, memory_profiling=
         #network_opt = dict(in_channel=4, out_channel=1, inner_channel=32, norm_groups=8, channel_mults=(1, 2, 4, 8, 8), attn_res=(16,8,), res_blocks=2, dropout = 0, with_attn=True, image_size=64, dim=3)
         #network_opt = dict(in_channel=4, out_channel=1, inner_channel=32, norm_groups=8, channel_mults=(1, 2, 4, 8, 8), attn_res=(8,), res_blocks=2, dropout = 0, with_attn=True, image_size=32, dim=3)
         #network = UNet
-        network_opt = dict(img_resolution=64, in_channels=4, out_channels=1, label_dim=1, # (for tokens?), augment_dim,
-                        model_channels=8, channel_mult=[2,2,2], attn_resolutions=[], #channel_mult_emb, num_blocks, attn_resolutions, dropout, label_dropout,
+        network_opt = dict(img_resolution=64, in_channels=4, out_channels=1, label_dim=0, # (for tokens?), augment_dim,
+                        model_channels=32, channel_mult=[2,2,2], attn_resolutions=[], #channel_mult_emb, num_blocks, attn_resolutions, dropout, label_dropout,
                         embedding_type='positional', channel_mult_noise=1, encoder_type='standard', decoder_type='standard', resample_filter=[1,1], 
                         )
         
@@ -417,17 +433,19 @@ def main(rank, world_size=0, total_epochs = 1, batch_size = 4, memory_profiling=
     #    SDE = VPSDE(beta_min=0.1, beta_max=20, N=1000)
 
         try:
-            fn = path + "/trained_models/model_11/DDPMpp_norm_lr_nocond_64_{0}_{1}".format(netG.noise_schedule_opt["schedule_type"], model_id)
+            fn = path + "/trained_models/model_11/DDPMpp_lr_standard_labeldim_{0}_64_{1}_{2}".format(netG.network_opt["label_dim"], netG.noise_schedule_opt["schedule_type"], model_id)
             netG.load_network(fn+".pth")
             print("Loaded network at {0}".format(fn), flush=True)
         except Exception as e:
             print(e, flush=True)
             print("Failed to load network at {0}. Starting from scratch.".format(fn+".pth"), flush=True)
 
-        
         train_data_module = CustomDataset(path_T21=path+"/outputs/T21_cubes_256/", path_IC=path+"/outputs/IC_cubes_256/", 
                                         redshifts=[10,], IC_seeds=list(range(0,56)), upscale=4, cut_factor=0, transform=False, norm_lr=True, device=device)
+        #train_data_module = CustomDataset(path_T21=path+"/outputs/T21_cubes_128/", path_IC=path+"/outputs/IC_cubes_128/", 
+        #                                redshifts=[10,], IC_seeds=list(range(1000,1008)), upscale=4, cut_factor=0, transform=False, norm_lr=True, device=device)
         #train_dataset, train_dataset_norm, train_dataset_extrema = train_data_module.getFullDataset()
+
         #train_dataloader = torch.utils.data.DataLoader( train_dataset, batch_size=batch_size, shuffle=False if multi_gpu else True, 
         #                                        sampler = DistributedSampler(train_dataset) if multi_gpu else None) #4
         #train_data_norm = torch.utils.data.DataLoader( train_dataset_norm, batch_size=batch_size, shuffle=False if multi_gpu else True, 
@@ -479,7 +497,9 @@ def main(rank, world_size=0, total_epochs = 1, batch_size = 4, memory_profiling=
 
 
         if (str(device)=="cuda:0") or (str(device)=="cpu"):
-            print("[{0}]: Epoch {1} in {2:.2f}s | loss min: {3:.4f}, loss history min: {4:.4f}, learning rate: {5:.3e}".format(str(device), len(netG.loss), time.time()-start_time, avg_loss,  torch.min(torch.tensor(netG.loss)).item(), netG.optG.param_groups[0]['lr']), flush=True)
+            print("[{0}]: Epoch {1} in {2:.2f}s | loss: {3:.3f}, mean(loss[-10:]): {4:.3f}, loss min: {5:.3f}, learning rate: {6:.3e}".format(str(device), len(netG.loss), time.time()-start_time, 
+                                                                                                                                              avg_loss,  torch.mean(torch.tensor(netG.loss[-10:])).item(), 
+                                                                                                                                              torch.min(torch.tensor(netG.loss)).item(), netG.optG.param_groups[0]['lr']), flush=True)
 
             #if e<4 and multi_gpu: #memory snapshot
             #    try:
@@ -509,21 +529,22 @@ def main(rank, world_size=0, total_epochs = 1, batch_size = 4, memory_profiling=
                 if rank==0:
                     print(f"[{device}] Not saving... validation loss={loss_validation:.2f} larger than validation minimum={loss_validation_min:.2f}", flush=True)
 
-        if False:#(avg_loss == torch.min(torch.tensor(netG.loss)).item()) and (len(netG.loss)>=1400):
-            netG.save_network( fn+".pth" )
-            #losses_validation, x_pred, k_and_dsq_and_idx = validation_step(netG=netG, validation_data=validation_data, validation_loss_type="dsq_voxel", device=device, multi_gpu=multi_gpu)
-            
-            if False: #(str(device)=="cuda:0") or (str(device)=="cpu"):
-                print("losses_validation: {0}, loss_validation minimum: {1}".format(losses_validation.item(), torch.min(torch.tensor(netG.loss_validation)).item()), flush=True)
+        if False:#len(netG.loss)>=150:
+            if (avg_loss == torch.min(torch.tensor(netG.loss)).item()) or len(netG.loss)%20==0:
+                netG.save_network( fn+".pth" )
+                #losses_validation, x_pred, k_and_dsq_and_idx = validation_step(netG=netG, validation_data=validation_data, validation_loss_type="dsq_voxel", device=device, multi_gpu=multi_gpu)
                 
-                if losses_validation.item() == torch.min(torch.tensor(netG.loss_validation)).item():       
-                    plot_checkpoint(validation_data, x_pred, k_and_dsq_and_idx=k_and_dsq_and_idx, epoch = e, path = fn+".png", device="cpu")
-                    netG.save_network( fn+".pth" )
-                else:
-                    print("Not saving model. Validaiton did not improve", flush=True)
+                if False: #(str(device)=="cuda:0") or (str(device)=="cpu"):
+                    print("losses_validation: {0}, loss_validation minimum: {1}".format(losses_validation.item(), torch.min(torch.tensor(netG.loss_validation)).item()), flush=True)
+                    
+                    if losses_validation.item() == torch.min(torch.tensor(netG.loss_validation)).item():       
+                        plot_checkpoint(validation_data, x_pred, k_and_dsq_and_idx=k_and_dsq_and_idx, epoch = e, path = fn+".png", device="cpu")
+                        netG.save_network( fn+".pth" )
+                    else:
+                        print("Not saving model. Validaiton did not improve", flush=True)
         
-        if e>=40:
-            if e%10==0 or avg_loss == torch.min(torch.tensor(netG.loss)).item():
+        if len(netG.loss)>=30:
+            if len(netG.loss)%8==0 or avg_loss == torch.min(torch.tensor(netG.loss)).item():
                 with netG.ema.average_parameters():
                     loss_validation = validation_step_v2(netG=netG, validation_dataloader=validation_dataloader, split_batch = True, device=device, multi_gpu=multi_gpu)
                 loss_validation_min = torch.min( torch.tensor(netG.loss_validation["loss_validation"]) ).item()
@@ -567,11 +588,11 @@ if __name__ == "__main__":
         print("Using multi_gpu", flush=True)
         for i in range(torch.cuda.device_count()):
             print("Device {0}: ".format(i), torch.cuda.get_device_properties(i).name)
-        mp.spawn(main, args=(world_size, 100000, 1, False, 17), nprocs=world_size) #wordlsize, total_epochs, batch size (for minibatch)
+        mp.spawn(main, args=(world_size, 100000, 1, False, 30), nprocs=world_size) #wordlsize, total_epochs, batch size (for minibatch)
     else:
         print("Not using multi_gpu",flush=True)
         try:
-            main(rank=0, world_size=0, total_epochs=1, batch_size=1, memory_profiling=False, model_id=17)#2*4)
+            main(rank=0, world_size=0, total_epochs=1, batch_size=1, memory_profiling=False, model_id=30)#2*4)
         except KeyboardInterrupt:
             print('Interrupted', flush=True)
             try:

models/utils.py
@@ -29,39 +29,25 @@ class CustomDataset(torch.utils.data.Dataset):
 
     @torch.no_grad()
     def __getitem__(self, idx):
-        #get full cubes
-        #T21 = torch.from_numpy(loadmat(self.path+ "/outputs/" + self.df[["T21"]].iloc[idx].values[0])["Tlin"]).unsqueeze(0).unsqueeze(0).to(torch.float32).to(self.device)
-        #delta = torch.from_numpy(loadmat(self.path + "/IC/" + self.df[["delta"]].iloc[idx].values[0])["delta"]).unsqueeze(0).unsqueeze(0).to(torch.float32).to(self.device)
-        #vbv = torch.from_numpy(loadmat(self.path+ "/IC/" + self.df[["vbv"]].iloc[idx].values[0])["vbv"]).unsqueeze(0).unsqueeze(0).to(torch.float32).to(self.device)
-        #T21_lr = torch.nn.functional.interpolate(T21, scale_factor=1/self.upscale, mode='trilinear')
-        T21 = torch.from_numpy(loadmat(self.path_T21 + self.df[["T21"]].iloc[idx].values[0])["Tlin"]).unsqueeze(0).to(torch.float32).to(self.device)
-        delta = torch.from_numpy(loadmat(self.path_IC + self.df[["delta"]].iloc[idx].values[0])["delta"]).unsqueeze(0).to(torch.float32).to(self.device)
-        vbv = torch.from_numpy(loadmat(self.path_IC + self.df[["vbv"]].iloc[idx].values[0])["vbv"]).unsqueeze(0).to(torch.float32).to(self.device)
-        T21_lr = torch.nn.functional.interpolate(T21.unsqueeze(0), scale_factor=1/self.upscale, mode='trilinear')[0]
-        labels = torch.tensor(self.df[["labels (z)"]].iloc[idx].values[0]).to(torch.float32).to(self.device)
-
-        if False:
-            if self.cut_factor > 0:
-                i = self.df["IC,z,subcube"].iloc[idx][-1]
-                T21 = get_subcubes(T21, self.cut_factor)[i:i+1]
-                delta = get_subcubes(delta, self.cut_factor)[i:i+1]
-                vbv = get_subcubes(vbv, self.cut_factor)[i:i+1]
-                T21_lr = get_subcubes(T21_lr, self.cut_factor)[i:i+1]
+        if True:
+            #for new data
+            T21 = torch.from_numpy(loadmat(self.path_T21 + self.df[["T21"]].iloc[idx].values[0])["Tlin"]).unsqueeze(0).to(torch.float32).to(self.device)
+            delta = torch.from_numpy(loadmat(self.path_IC + self.df[["delta"]].iloc[idx].values[0])["delta"]).unsqueeze(0).to(torch.float32).to(self.device)
+            vbv = torch.from_numpy(loadmat(self.path_IC + self.df[["vbv"]].iloc[idx].values[0])["vbv"]).unsqueeze(0).to(torch.float32).to(self.device)
+            T21_lr = torch.nn.functional.interpolate(T21.unsqueeze(0), scale_factor=1/self.upscale, mode='trilinear')[0]
+            labels = torch.tensor(self.df[["labels (z)"]].iloc[idx].values[0]).to(torch.float32).to(self.device)
+        else:
+            #for old data
+            #use getFullDataset first to set self.dataset
+            T21, delta, vbv, T21_lr, labels = self.dataset.tensors
             
-            T21, min_max_T21 = normalize(T21)
-            delta, min_max_delta = normalize(delta)
-            vbv, min_max_vbv = normalize(vbv)
-            T21_lr, min_max_T21_lr = normalize(T21_lr)
+            T21 = T21[idx]
+            delta = delta[idx]
+            vbv = vbv[idx]
+            T21_lr = T21_lr[idx]
+            labels = labels[idx]
 
-            if self.transform: #rotate
-                print("Transforms disabled. Rotation augmentation in training loop", flush=True)
-                #T21,delta,vbv,T21_lr = augment_dataset(T21, delta, vbv, T21_lr, n=1)
 
-            #select [0] so iterator can batch
-            T21 = T21[0]
-            delta = delta[0]
-            vbv = vbv[0]
-            T21_lr = T21_lr[0]
     
         return T21, delta, vbv, T21_lr, labels
     
@@ -142,6 +128,7 @@ class CustomDataset(torch.utils.data.Dataset):
         vbv_norm, vbv_extrema = normalize(vbv)
 
         dataset = torch.utils.data.TensorDataset(T21, delta, vbv, T21_lr, labels)
+        self.dataset = dataset #save dataset for later use
         dataset_norm = torch.utils.data.TensorDataset(T21_norm, delta_norm, vbv_norm, T21_lr_norm, labels)
         dataset_extrema = torch.utils.data.TensorDataset(T21_extrema, delta_extrema, vbv_extrema, T21_lr_extrema)
         return dataset, dataset_norm, dataset_extrema
@@ -323,18 +310,6 @@ def calculate_power_spectrum(data_x, Lpix=3, kbins=100, dsq = False, method="tor
     else:
         raise ValueError("Method must be numpy or torch")
 
-@torch.no_grad()
-def normalize(x, x_min=None, x_max=None):
-    if x_min is None and x_max is None:
-        x_min = torch.amin(x, dim=(1,2,3,4), keepdim=True)
-        x_max = torch.amax(x, dim=(1,2,3,4), keepdim=True)
-    
-    x_extrema = torch.cat([x_min, x_max], dim=1)
-    
-    x = (x - x_min) / (x_max - x_min)
-    x = 2 * x - 1
-
-    return x, x_extrema
 
 def invert_normalization(x, x_extrema):
     x_min = x_extrema[:,:1]
@@ -363,39 +338,40 @@ def get_subcubes(cubes, cut_factor=0):
     else:
         return cubes
     
-if __name__ == "__main__":
-    import time 
-
-    path = os.getcwd().split("21cmGen")[0]+"21cmGen"
-
-    train_data_module = CustomDataset(path_T21=path+"/outputs/T21_cubes_256/", path_IC=path+"/outputs/IC_cubes_256/", 
-                                      redshifts=[10,], IC_seeds=list(range(0,4)), upscale=4, cut_factor=1, transform=False, norm_lr=True, device="cpu")
-    
-    print(train_data_module.df)
-    
-    if True:        
-        start_time0 = time.time()
-        train_dataset, train_dataset_norm, train_dataset_extrema = train_data_module.getFullDataset()
-        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True)
-        print(f"Time loading full dataset: {(time.time()-start_time0):.2f}, shape: {train_dataset.tensors[0].shape}", flush=True)
-        if False:
-            for i,(T21, delta, vbv, T21_lr, labels) in enumerate(train_dataloader):
-                multiple_redshifts = False
-                cut_factor = torch.randint(low=0, high=3, size=(1,))
-                cut_label = torch.ones_like(labels) * 1/(2**cut_factor)**3
-                labels = torch.cat([labels, cut_label], dim=1) if multiple_redshifts else cut_label
+@torch.no_grad()
+def normalize(x, mode = "minmax", **kwargs):
+    if mode == "minmax":
+        if "x_min" not in kwargs: 
+            x_min = torch.amin(x, dim=(1,2,3,4), keepdim=True)
+        else:
+            x_min = kwargs["x_min"]
+        if "x_max" not in kwargs:
+            x_max = torch.amax(x, dim=(1,2,3,4), keepdim=True)
+        else:
+            x_max = kwargs["x_max"]
         
+        x_extrema = torch.cat([x_min, x_max], dim=1)
+        
+        x = (x - x_min) / (x_max - x_min)
+        x = 2 * x - 1
+        return x, x_extrema
     
-    #print(get_subcubes(train_dataset.tensors[0], cut_factor=torch.tensor([1])).shape)
-    if False:
-        train_dataloader = torch.utils.data.DataLoader(train_data_module, batch_size=2, shuffle=True)
-        for T21, delta, vbv, T21_lr, labels in train_dataloader:
-            print(labels)
-        print(f"Time: {(time.time()-start_time):.2f}", flush=True)
+    elif mode == "standard":
+        if "x_mean" not in kwargs:
+            x_mean = torch.mean(x, dim=(1,2,3,4), keepdim=True)
+        else:
+            x_mean = kwargs["x_mean"]
+        if "x_std" not in kwargs:
+            x_std = torch.std(x, dim=(1,2,3,4), keepdim=True)
+        else:
+            x_std = kwargs["x_std"]
 
-    #import time
-    #start_time = time.time()
+        x_stats = torch.cat([x_mean, x_std], dim=1)
 
-    #data = loadmat(path + "/outputs/T21_cubes_256/T21_cube_6__IC6.mat")
+        x = (x - x_mean) / (2 * x_std) #should the 2 be there? LR std approx. 0.5*HR std
+
+        return x, x_stats
+
     
-    #print("Loading time: ", time.time()-start_time)
+if __name__ == "__main__":
+    pass